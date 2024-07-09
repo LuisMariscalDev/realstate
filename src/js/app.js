5208317e9ef7fb2aec8fd51b9a392c031473cc5d
Luis Mariscal <luisferdev16@gmail.com>
2024-07-08 18:58:41 -0600

    modo oscuro

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     eventListeners();
+    darkMode();
 });
+
+function darkMode() {
+
+    const preferDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    console.log(preferDarkMode.matches);
+
+    if(preferDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    preferDarkMode.addEventListener('change', function() {
+        if(preferDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+    })
+}
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
